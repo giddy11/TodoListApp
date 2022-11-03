2e5560f52d6347314fad8421f00926b9b189aa16
giddy11 <edoghotugiddy@gmail.com>
2022-11-03 16:55:00 +0100

    Todo Textblock

---
+++ TODO_ListApp/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO_ListApp.ViewModels
+{
+    public class TodoListViewModel : ViewModelBase
+    {
+        private string myVar1;
+
+        public string MyProperty1
+        {
+            get { return myVar; }
+            set { myVar = value; OnPropertyChanged(); }
+        }
+
+        private string myVar;
+
+        public string MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; OnPropertyChanged(); }
+        }
+
+
+    }
+}
---
+++ TODO_ListApp/Views/TodoListView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using TODO_ListApp.ViewModels;
+
+namespace TODO_ListApp.Views
+{
+    /// <summary>
+    /// Interaction logic for TodoListView.xaml
+    /// </summary>
+    public partial class TodoListView : UserControl
+    {
+        public TodoListView()
+        {
+            InitializeComponent();
+            DataContext = new TodoListViewModel();
+        }
+    }
+}
